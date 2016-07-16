c09430534a1085ecc52f438a0516dc7e6077516e
apiconz <picondev@gmail.com>
2016-07-16 12:58:13 -0500

    Change design changes Added add note fragment dialog

--- app/src/main/java/com/devpicon/android/milibreta/holders/NoteViewHolder.java
+++ app/src/main/java/com/devpicon/android/milibreta/holders/NoteViewHolder.java
@@ -2,7 +2,13 @@ package com.devpicon.android.milibreta.holders;
 
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.devpicon.android.milibreta.R;
+
+import jp.wasabeef.glide.transformations.CropCircleTransformation;
 
 /**
  * Created by armando on 7/15/16.
@@ -15,14 +21,28 @@ public class NoteViewHolder extends RecyclerView.ViewHolder {
         this.view = itemView;
     }
 
-    public void setText(String text){
-        TextView textViewName = (TextView) view.findViewById(android.R.id.text1);
+    public void setText(String text) {
+        TextView textViewName = (TextView) view.findViewById(R.id.txt_item_note);
         textViewName.setText(text);
     }
 
-    public void setName(String name){
-        TextView textViewName = (TextView) view.findViewById(android.R.id.text2);
+    public void setName(String name) {
+        TextView textViewName = (TextView) view.findViewById(R.id.txt_item_name);
         textViewName.setText(name);
+    }
+
+    public void setTimestamp(String timestamp) {
+        TextView textViewTimestamp = (TextView) view.findViewById(R.id.txt_item_timestamp);
+        textViewTimestamp.setText(timestamp);
+    }
+
+    public void setImage(String url) {
+        ImageView imageViewAvatar = (ImageView) view.findViewById(R.id.img_item_avatar);
+        Glide.with(view.getContext())
+                .load(url)
+                .asBitmap()
+                .transform(new CropCircleTransformation(view.getContext()))
+                .into(imageViewAvatar);
     }
 
 }
